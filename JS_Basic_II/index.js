65af4f62c48c94f5e9585ad369cf896df4367350
raishivamm <shrai0432@gmail.com>
2023-08-23 15:30:26 +0530

    Added JS_bsic2

--- JS_Basic_II/index.js
+++ JS_Basic_II/index.js
@@ -1,52 +1,105 @@
-console.log("Lets start");
+console.log('Lets start');
+object creation
+const rectangle = {
+    length:1,bredth:2,
 
-// let rectangle={
-//     length:1,
-//     breadth:2,
+    draw:function(){
+        console.log('draw');
+    }
+};
 
-//     draw: function() {
-//         console.log('draw');
-//     }
-// };
+// Factory function
 
-// factory function
-
-function createRectangle(len,bre) {
-
-    let rectangle={
-        length : len,
-        breadth : bre,
+function createRectangle(len,bre){
+    return rectangle = {
+        length:len,
+        bredth:bre,
     
-        draw: function() {
+        draw:function(){
             console.log('draw');
         }
     };
     return rectangle;
-
 }
-let rectangleobj1 = createRectangle(5,4);
 
-// constructor function
-function Rectangle(len,bre) {
-    this.length=len;
-    this.breadth=bre;
+let rectangleObj1 = createRectangle(5,6);
 
-    this.draw = function() {
+// constructor function -> PascalNotation
+// Constructor function -> props/methods -> initialise/define
+
+function Rectangle(len,bre){
+    this.length =len;
+    this.breadth =bre;
+    this.draw = function(){
         console.log('Draw rectangle');
-
     }
 }
 
-let rectangleObject = new Rectangle(4,6);
+let rectangleObject = new Rectangle(10,15);
 
-rectangleObject.color='yellow';
+rectangleObject.color ='blue';
+delete rectangleObject.color;
 
-let rectangle= {
-    length:4,
-    bredth:5,
+let a =10;
+let b=a;
+a++;
+console.log(a);
+console.log(b);
+
+let a = {
+    value:12,
+};
+let b=a;
+a.value++;
+console.log(a.value);
+console.log(b.value);
+
+
+let Rectangle ={
+    length:3,
+    brefth:4
+};
+for(let key in Rectangle){
+    console.log(key,Rectangle[key ]);
+}
+
+OBJECT CLONING
+
+let src = {
+    a:10,
+    b:20
 };
 
-for(let key in rectangle){
-    console.log(key,rectangle[key]);
+let dest ={};
+
+iteration
+
+for (let key in src){
+    dest[key] = src[key];
 }
+
+assign
+
+let src = {
+    a:30,
+    b:50
+};
+
+let src2 = {
+    value:25
+};
+
+let dest = Object.assign({},src,src2);
+
+spread
+
+let src = {
+    a:29,
+    b:30
+};
+let dest={...src};
+
+
+
+
 
